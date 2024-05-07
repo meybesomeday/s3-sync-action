4e6910a2e3420602c68181f13b86ce4ca59e7fda
meybesomeday <kountryspring@gmail.com>
2024-05-07 11:47:56 -0500

    fix using environment variable in awk

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,7 +58,7 @@ text
 EOF
 
 echo "$data"
-list=$(echo "$data" | awk '/^.*delete:/ { gsub(/^.*\*\*\*/, ENVIRON["BASE_URL"], $3); \
+list=$(echo "$data" | awk -v BASE_URL=${BASE_URL} '/^.*delete:/ { gsub(/^.*\*\*\*/, BASE_URL, $3); \
                           URLS[KEY]=$3; KEY++ } END { for (KEY in URLS) \
                           printf("%s,", URLS[KEY]) }')
 echo "$list"
