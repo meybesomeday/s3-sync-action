ca6ce0efacbf4092a5f0370a969d4a15641a27c0
meybe <153315487+meybesomeday@users.noreply.github.com>
2024-05-06 20:14:38 -0500

    add \

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,7 +42,7 @@ EOF
 data=$(sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
               --no-progress \
-              --dryrun
+              --dryrun \
               ${ENDPOINT_APPEND} $*")
 echo "data=$data" >> $GITHUB_OUTPUT
 
